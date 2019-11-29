28c95cd570a4ff918e176dc3e96294046357f1a4
Camila Melo <mcmelo@H6572.ad.hugeinc.com>
2019-11-29 10:57:54 -0500

    babel config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,30 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
 	mode: 'development',
 	entry: './src/index.js',
+	externals: [nodeExternals()],
+	module: {
+		rules: [
+			{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+			{
+				test: /\.css$/,
+				use: [
+					"style-loader",
+					"css-loader"
+				]
+			}
+		],
+	},
 	plugins: [
 		new CleanWebpackPlugin(),
 		new HtmlWebpackPlugin({
@@ -17,16 +37,5 @@ module.exports = {
 	output: {
 		filename: '[name].[chunkhash].js',
 		path: path.resolve(__dirname, 'dist'),
-	},
-	module: {
-		rules: [
-			{
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          "css-loader"
-        ]
-      }
-		],
 	},
 };
